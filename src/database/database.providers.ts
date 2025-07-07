87ca3258495978ae5595997223a365a4779d5ace
StasivPavlo <pavlostasiv@gmail.com>
2025-07-07 21:51:03 +0200

    test deploy

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -5,13 +5,8 @@ export const databaseProviders = [
   {
     provide: 'SEQUELIZE',
     useFactory: async () => {
-      const sequelize = new Sequelize({
+      const sequelize = new Sequelize(process.env.POSTGRES_HOST || '', {
         dialect: 'postgres',
-        host: process.env.POSTGRES_HOST,
-        port: Number(process.env.POSTGRES_PORT),
-        username: process.env.POSTGRES_USER,
-        password: process.env.POSTGRES_PASSWORD,
-        database: process.env.POSTGRES_DB,
       });
       sequelize.addModels([Patient]);
       await sequelize.sync();
